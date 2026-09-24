3237a9407ce01c49b63691f3c392c5c82c1028b5
638shivani <shivanimetri143@gmail.com>
2026-09-24 20:04:19 +0530

    Update comment to clarify rectangle area calculation

---
+++ Class_1/prog_4.cpp
@@ -0,0 +1,21 @@
+//finding the area of rectangle using user input
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+
+    float length;
+    float width;
+    float area;
+
+ cout<< " enter the length of rec";
+ cin>>length;
+ cout<<"enter the width of rec";
+ cin>>width;
+
+    area = length*width;
+ cout<<"the area of rec" << area;
+ return 0;
+}
