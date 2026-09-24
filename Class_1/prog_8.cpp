b2ec979ce6ec5d4152c7968d55f53aa37bdd8161
638shivani <shivanimetri143@gmail.com>
2026-09-24 21:28:31 +0530

    Create prog_8.cpp

---
+++ Class_1/prog_8.cpp
@@ -0,0 +1,22 @@
+//find the length of the string
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    string s;
+
+
+    string rev = "";
+
+    cout<<"enter the string:";
+    cin>>s;
+    string word = s;
+
+    int len=s.length();
+    cout<<"the length of string is:" <<len<<endl;
+    return 0;
+
+
+}
