5c86bed08c2df4fc8ffe4bba1cdd78453d550760
638shivani <shivanimetri143@gmail.com>
2026-09-24 20:02:41 +0530

    Create prog_3.cpp

---
+++ Class_1/prog_3.cpp
@@ -0,0 +1,15 @@
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int grade;
+    char age;
+    float cgpa;
+
+    cout<<"Grade: "<<grade<<endl;
+    cout<<"Age  : "<<age<<endl;
+    cout<<"CGPA : "<<cgpa<<endl;
+
+}
