20d4c7bcb64a5e4e818375cc434098f165cf2415
638shivani <shivanimetri143@gmail.com>
2026-09-24 21:37:38 +0530

    Update prog_1.cpp

--- Class_2/prog_1.cpp
+++ Class_2/prog_1.cpp
@@ -1,1 +1,38 @@
+//create the class and oblective accesing the private data through member function using user in take
 
+#include<iostream>
+using namespace std;
+
+class student
+{
+private:
+    int marks;
+    float sgpa;
+public:
+
+    void setData()
+    {
+      cout<<"enter the marks"<<endl;
+      cin>>marks;
+      cout<<"enter the sgpa"<<endl;
+      cin>>sgpa;
+
+
+     //marks=a;
+     //sgpa=s;
+    }
+    getData()
+    {
+
+        cout<<"the marks"<<marks <<endl;
+        cout<<"the sgpa"<<sgpa<<endl;
+    }
+};
+
+int main()
+{
+    student s1;
+    s1.setData();
+    s1.getData();
+    return 0;
+}
